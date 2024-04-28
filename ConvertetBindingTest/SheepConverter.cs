fa49550d6c9201a023f1994bcd9de2dde4be9d2d
sheephuman <shin.citysheep@gmail.com>
2024-04-29 01:07:04 +0900

    プロジェクト ファイルを追加します。

---
+++ ConvertetBindingTest/SheepConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ConvertetBindingTest
+{
+    // YourConverter.cs
+    public class SheepConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string result = string.Empty;
+
+            result = values[0].ToString() + values[1].ToString()
+                + values[2].ToString();
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConvertetBindingTest/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertetBindingTest
+{
+    // ViewModel.cs
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _inputA = string.Empty;
+        public string InputA
+        {
+            get => _inputA;
+            set
+            {
+                _inputA = value;
+                OnPropertyChanged(nameof(InputA));
+            }
+        }
+
+        private string _inputB = string.Empty;
+        public string InputB
+        {
+            get => _inputB;
+            set
+            {
+                _inputB = value;
+                OnPropertyChanged(nameof(InputB));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
